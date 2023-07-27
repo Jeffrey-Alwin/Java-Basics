4f3de09ffcb7e7989ffa8c85960d3e16d40e7a3a
Jeffrey-Alwin <jeffreyalwin7@gmail.com>
2023-07-27 10:23:33 +0530

    initial message

---
+++ src/Packageone/Assignment02.java
@@ -0,0 +1,46 @@
+package Packageone;
+
+public class Assignment02 {
+   
+	public int Addition(int a, int b) {
+		
+		int c= a+b ;
+		System.out.println("Addition of "+ c);
+		return c;
+	}
+public int Multiplication(int a, int b) {
+		
+		int c= a*b ;
+		System.out.println("Multiplication of "+ c);
+		return c;
+	}
+public int Subtraction(int a, int b) {
+	
+	int c= a-b ;
+	System.out.println("Subtraction of "+ c);
+	return c;
+}
+public int division(int a, int b) {
+	
+	int c= a/b ;
+	System.out.println("Division of "+ c);
+	return c;
+}
+
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		 Assignment02 Calculation = new Assignment02();
+	       
+	       Calculation.division(10, 50);
+	}
+	
+}
+
+
+
+
+
